027c5ac54f00ea3affe4d5f180afb08ab09fd391
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2022-12-09 08:50:19 +0000

    Deploying to gh-pages from @ smirnypavel/my-site@0befe4a383c50cb5048da67478e5c8557228c34d 🚀

---
+++ index.ca26020a.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$("form").submit((function(){var t=$(this);return $.ajax({type:"POST",url:"mail.php",data:t.serialize()}).done((function(){alert("Thank you!"),setTimeout((function(){t.trigger("reset")}),1e3)})),!1}))}));
+//# sourceMappingURL=index.ca26020a.js.map
--- index.f59bd23e.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$("form").submit((function(){var t=$(this);return $.ajax({type:"POST",url:"./src/mail.php",data:t.serialize()}).done((function(){alert("Thank you!"),setTimeout((function(){t.trigger("reset")}),1e3)})),!1}))}));
-//# sourceMappingURL=index.f59bd23e.js.map
