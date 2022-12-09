3dde55b0f377e002496bb49392d0b03736deb365
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2022-12-09 00:51:22 +0000

    Deploying to gh-pages from @ smirnypavel/my-site@330de012de50d32516a2e7350cf09594f062968f 🚀

---
+++ index.de91d2e5.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$("form").submit((function(){var t=$(this);return $.ajax({type:"POST",url:"payalnic@ukr.net",data:t.serialize()}).done((function(){alert("Thank you!"),setTimeout((function(){t.trigger("reset")}),1e3)})),!1}))}));
+//# sourceMappingURL=index.de91d2e5.js.map
